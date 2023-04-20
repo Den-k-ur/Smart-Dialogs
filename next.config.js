eb58dd8082e6444455cbd2b9ebb7078fea7b765f
denis.kurganov <denis.kurganov@osinit.com>
2023-04-20 15:31:49 +0200

    add display svg images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const commonRuntimeConfig = {
   reactStrictMode: true,
+};
+
+const nextConfig = {
+  // reactStrictMode: true,
+  swcMinify: true,
+  env: commonRuntimeConfig,
+  poweredByHeader: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  ...(commonRuntimeConfig.ENV === 'developmennt' ? {} : { output: 'standalone' }),
 };
 
 module.exports = nextConfig;
